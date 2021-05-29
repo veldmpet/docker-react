b1b5a962083cbfc2b0605766266f3a707d8e0326
Peter Veldman <peter.veldman@tieto.com>
2021-05-29 14:41:12 +0200

    added travis deploy config2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there again
+          Hi there again and again
         </p>
         <a
           className="App-link"
